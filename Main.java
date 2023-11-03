23e7fcab5614bd844736cf3a90f3987ae230c9c0
VighneshMestry27 <149404787+VighneshMestry27@users.noreply.github.com>
2023-11-03 07:32:27 +0530

    Done

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = args[0];
+        String b = args[1];
+        int c = Integer.parseInt(a);
+        int d = Integer.parseInt(b);
+        System.out.println(c + d);
+    }
+}
